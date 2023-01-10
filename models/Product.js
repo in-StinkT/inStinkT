045dc7c3d6fb27cc09e1d0fa57e9d0564c85213e
brandon hicks <brandonhicks751@gmail.com>
2023-01-10 02:58:00 -0600

    add basic models for Product and User

---
+++ models/Product.js
@@ -0,0 +1,39 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL(13,2),
+      allowNull: false
+    },
+    manufacturer: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product'
+  }
+);
+
+module.exports = Product;
