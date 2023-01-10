8440d091b56452f46588f2f3a454d6124554a2bc
brandon hicks <brandonhicks751@gmail.com>
2023-01-10 03:10:44 -0600

    add api routes for user table

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+// const productRoutes = require('./productRoutes');
+const userRoutes = require('./userRoutes');
 
-
+router.use('/users', userRoutes);
 
 module.exports = router;
