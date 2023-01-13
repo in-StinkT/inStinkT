ccd088b60fe5fdc69752b555015bf44fc15577e5
brandon hicks <brandonhicks751@gmail.com>
2023-01-13 03:09:57 -0600

    added form validation for login and register views

--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,24 @@
+const isEmail = (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+const validate = (email, password, passwordConfirm) => {
+  err = '';
+
+  if(password.length < 8 || passwordConfirm.length < 8) {
+    err += 'Password must be 8 or more characters long.\n'
+  }
+
+  if(!isEmail(email)) {
+    err += 'Email must be valid\n'
+  }
+
+  if(err.length > 0) {
+    alert(err);
+    return false;
+  }
+
+  return true;
+}
+
 const registrationHandler = async (e) => {
   e.preventDefault();
 
@@ -8,12 +29,20 @@ const registrationHandler = async (e) => {
   const passwordConfirm = document.querySelector('#passwordConfirmInput').value.trim();
 
   if(email && first_name && last_name && password && passwordConfirm && password === passwordConfirm) {
+
+    if(!validate(email, password, passwordConfirm)) {
+      return;
+    }
+
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({email, first_name, last_name, password}),
       headers: {'Content-Type': 'application/json'}
     });
     
+    if(response.ok) {
+      document.location.replace('/')
+    }
   }
 }
 
