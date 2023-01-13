893263217c549a7c5bf40126856d28f913966839
brandon hicks <brandonhicks751@gmail.com>
2023-01-12 20:47:16 -0600

    add basic product routes and views

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api')
+const productRoutes = require('./product-routes')
 
-router.use('/api', apiRoutes)
+router.use('/api', apiRoutes);
+router.use('/product', productRoutes);
 router.use('/', homeRoutes);
 
 module.exports = router;
