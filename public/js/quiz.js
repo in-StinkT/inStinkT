154376c2a347212dd673b2c37219418413aef4a7
mintedd <xxxxiheartyouxxxxx@gmail.com>
2023-01-19 21:49:52 -0500

    added photo for quiz ans choices

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,3 +1,20 @@
+var questions = [
+    {
+        question: 'Which do you prefer?',
+        options: ['Amber/Wood', 'Citrus'],
+    },
+    {
+        question: 'Which do you prefer?',
+        options: ['Amber/Wood', 'Citrus'],
+    },
+]
+
+var count = 0;
+
 function startQuiz () {
     
 }
+
+document
+    .querySelector('.start-quiz')
+    .addEventListener('submit', startQuiz)
