d9e3c95000e36d77201b053ec3382e3d97ff9862
mintedd <xxxxiheartyouxxxxx@gmail.com>
2023-01-21 16:36:24 -0500

    dont merge this to main

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -9,12 +9,36 @@ var questions = [
     },
 ]
 
-var count = 0;
+var countFloral = 0;
+var countSpice = 0;
+var countCitrus = 0;
+var countWood = 0;
+var countFruity = 0;
+var countFresh = 0;
+
+
+// $(".startBtn").onClick(function () {
+//     // $('#questions').show();
+//     // $('#intro').hide();
+// });
+
+var answerEl = document.querySelector('.choices');
 
 function startQuiz () {
-    
+    const questionArr = questions[i];
+    var questionChoices= document.querySelector('.question');
+    questionChoices.textContent = questionArr.question;
+    answerEl.innerHTML = '';
+    for (let i = 0; i < questionArr.options.length; i++) {
+        const  answerOptions = questionArr.options[i];
+        var btnOption = document.createElement('button');
+        btnOption.setAttribute('class', 'opt1');
+        btnOption.setAttribute('value', answerOptions);
+        btnOption.textContent = i + 1 + answerOptions;
+        answerEl.append(btnOption);
+    }
 }
 
 document
-    .querySelector('.start-quiz')
+    .querySelector('.startBtn')
     .addEventListener('submit', startQuiz)
